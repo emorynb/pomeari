47a6b2d57c7b19155ede76fbe2ea6bccc7862b12
Emory Fontana <emory.enby@gmail.com>
2026-08-29 19:00:33 +0700

    Remove debug printing for LongBlankPlatform

--- src/pomeari/platforms/longblank/platform.py
+++ src/pomeari/platforms/longblank/platform.py
@@ -12,8 +12,6 @@ class LongBlankPlatform(Platform):
     info = ModuleInfo(title="Long&Blank")
 
     async def post_long(self, post: Post, config: dict[str, Any]) -> XpostResult:
-        print(post)
-        print(post.metadata)
         id = "".join(["{}".format(randint(0, 9)) for num in range(0, 10)])
         created_at = datetime.today().isoformat()
         return XpostResult(url=f"https://longblank.local/{id}", created_at=created_at)
